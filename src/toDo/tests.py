a22abd36f3702a5dfe67647b85fa1780d32aeea2
AliGorithm <alisher.coder@gmail.com>
2018-05-02 14:38:09 -0500

    FewTestsAreAddedForCreatingAList

--- src/toDo/tests.py
+++ src/toDo/tests.py
@@ -1,6 +1,48 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from toDo.models import ToDoItem
 
-from django.test import TestCase
+# Create tests here
+def createItem(client):
+    url = reverse('todoitem-list')
+    data = {'title': 'Walk the dog'}
+    return client.post(url, data, format='json')
 
-# Create your tests here.
+class TestCreateTodoItem(APITestCase):
+    """
+    Ensure we can create a new todo ToDoItem
+    """
+    def setUp(self):
+        self.response = createItem(self.client)
+
+    def test_received_201_created_status_code(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_reveived_location_header_hyperlink(self):
+        self.assertRegexpMatches(self.response['Location'], '^http://.+/todos/[\d]+$')
+
+    def test_item_was_created(self):
+        self.assertEqual(ToDoItem.objects.count(), 1)
+
+    def test_item_has_correct_title(self):
+        self.assertEqual(ToDoItem.objects.get().title, 'Walk the dog')
+
+
+class TestUpdateTodoItem(APITestCase):
+    """
+    Ensure we can update an existing todo item using PUT
+    """
+
+    def setUp(self.client):
+        response.createItem(self.client)
+        self.assertEqual(ToDoItem.objects.get().completed, False)
+        url = response['Location']
+        data = {'title': 'Walk the dog', 'completed': True}
+        self.response = self.client.put(url, data, format='json')
+
+    def test_received_200_created_status_code(self):
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+
+    def test_item_was_updated(self):
+        self.assertEqual(ToDoItem.objects.get().completed, True)                  
