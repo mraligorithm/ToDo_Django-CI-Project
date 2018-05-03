a579febc85250159d1b21feecdc3734430ec2fcd
AliGorithm <alisher.coder@gmail.com>
2018-05-02 20:53:00 -0500

    TestIngOuputUpdated

---
+++ src/todobackend1/settings/test.py
@@ -0,0 +1,33 @@
+from base import *
+import os
+
+
+# Installed App
+INSTALLED_APPS += ('django_nose',)
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+'--verbosity=2', # Verbose ouput
+'--nologcapture', # dont out put log capture
+'--with-coverage', # activate coverage report
+'--cover-package=toDo', # coverage reports will apply to these packages
+'--with-spec', # spec style tests
+'--spec-color', #
+'--with-xunit',
+'--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
+'--cover-xml', # produce XML coverage info
+'--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
+]
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todobackend1'),
+        'USER': os.environ.get('MYSQL_USER', 'toDo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+    }
+}
