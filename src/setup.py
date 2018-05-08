45505110a7dc8c5c6ae151acae89a6e6bab94b5d
AliGorithm <alisher.coder@gmail.com>
2018-05-08 09:56:40 -0500

    dockerC Builder cant run

--- src/setup.py
+++ src/setup.py
@@ -7,18 +7,18 @@ setup (
   packages              = find_packages(),
   include_package_data  = True,
   scripts               = ["manage.py"],
-  install_requires      = ["Django==1.11.13",
-                           "django-cors-headers==2.2.0",
-                           "djangorestframework==3.8.2",
-                           "MySQL-python==1.2.5",
-                           "pytz==2018.4"],
+  install_requires      = ["Django>=1.11.13",
+                           "django-cors-headers>=2.2.0",
+                           "djangorestframework>=3.8.2",
+                           "MySQL-python>=1.2.5",
+                           "pytz>=2018.4"],
   extras_require        = {
                               "test": [
-                                 "colorama==0.3.9",
-                                 "coverage==4.5.1",
-                                 "django-nose==1.4.5",
-                                 "nose==1.3.7",
-                                 "pinocchio==0.4.2"
+                                 "colorama>=0.3.9",
+                                 "coverage>=4.5.1",
+                                 "django-nose>=1.4.5",
+                                 "nose>=1.3.7",
+                                 "pinocchio>=0.4.2"
                               ]
                            }
 )
